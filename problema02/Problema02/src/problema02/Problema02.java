4014fc984f5bcdd6131d2f457fa463c5dcb34834
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-11-16 16:54:25 -0500

    Cambios

--- problema02/Problema02/src/problema02/Problema02.java
+++ problema02/Problema02/src/problema02/Problema02.java
@@ -22,7 +22,47 @@ public class Problema02 {
         entrada.useLocale(Locale.US);
         
         String propietario;
+        int tipo;
+        double valorVehiculo;
+        double peaje = 0.0;
+        String tipoVehiculo = "";
+                
+        System.out.println("Ingrese el nombre del propietario:");
+        propietario = entrada.nextLine();
+
+        System.out.println("Ingrese el numero según su tipo de vehículo:\n"
+                + "(1: Vehículo liviano particular\n"
+                + " 2: Vehículo grande particular\n"
+                + " 3: taxi\n"
+                + " 4: bus urbano)");
+        tipo = entrada.nextInt();   
+
+        System.out.println("Ingrese el valor del vehículo");
+        valorVehiculo = entrada.nextDouble();
         
+        switch (tipo) {
+            case 1: // Vehículo liviano particular
+                tipoVehiculo = "vehículo liviano particular";
+                peaje = (valorVehiculo * 0.0001) + 2;
+                break;
+            case 2: // Vehículo grande particular
+                tipoVehiculo = "vehículo grande particular";
+                peaje = (valorVehiculo * 0.0002) + 2.5;
+                break;
+            case 3: // Taxi
+                tipoVehiculo = "taxi";
+                peaje = (valorVehiculo * 0.0004) + 1.5;
+                break;
+            case 4: // Bus urbano
+                tipoVehiculo = "bus urbano";
+                peaje = (valorVehiculo * 0.0005) + 2.2;
+                break;
+            default:
+                System.out.println("Tipo de vehículo no reconocido.");
+        }
+        System.out.println("\nPeaje 'Buena vía'\n\t" + "Propietario: " 
+                + propietario + "\nTipo: " + tipoVehiculo + "\n\tValor: $" + valorVehiculo 
+                + "\n\tPeaje: $" + peaje);
     }
     
 }
